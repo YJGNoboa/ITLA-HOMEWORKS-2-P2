bfbe19b74972a6512d42cce6859d4c70e5c41d71
Yanel Gonzalez <yanelgonzalez752@gmail.com>
2026-02-14 14:19:21 -0800

    Commit

---
+++ SkillsTracker/SkillsTrackerAPI/Models/Entities/Skill.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkillsTrackerAPI.Models.Entities
+{
+    public class Skill
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(250)]
+        public string? Description { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        public List<SkillProgress> SkillProgresses { get; set; } = new();
+    }
+}
+        
+    
+
+        
+    
+
+
+
---
+++ SkillsTracker/SkillsTrackerAPI/Models/Entities/SkillProgress.cs
@@ -0,0 +1,30 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace SkillsTrackerAPI.Models.Entities
+{
+    public class SkillProgress
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int SkillId { get; set; }
+        public Skill Skill { get; set; } = null!;
+
+        [Range(1, 100)]
+        public int Level { get; set; }
+
+        public int ExperiencePoints { get; set; }
+
+        public DateTime ProgressDate { get; set; } = DateTime.Now;
+
+        [MaxLength(250)]
+        public string? Notes { get; set; }
+    }
+}
+
+
+
+
+
